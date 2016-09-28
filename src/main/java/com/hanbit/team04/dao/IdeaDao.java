29a7d39191207b9ece286d4518cc9173132853d4
hb5026 <hb5026@hb5026-PC>
2016-09-28 15:18:21 +0900

    뇨내

--- src/main/java/com/hanbit/team04/dao/IdeaDao.java
+++ src/main/java/com/hanbit/team04/dao/IdeaDao.java
@@ -1,64 +1,51 @@
 package com.hanbit.team04.dao;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.hanbit.team04.core.IdeaVo;
 
-
+@Repository
 public class IdeaDao {
 	private static final Logger LOGGER = LoggerFactory.getLogger(IdeaDao.class);
 
 	@Autowired
 	private SqlSession sqlSession;
 
-	public int insertIdea(IdeaVo Idea) {
+	public int insertIdea(IdeaVo Idea) {// 게시글 등록
 		LOGGER.debug("insertIdea");
-
 		int result = sqlSession.insert("Idea.insertIdea", Idea);
 
 		return result;
 	}
 
-	public int updateIdea(IdeaVo Idea) {
+	public int updateIdea(IdeaVo Idea) {// 게시글 수정
 		int result = sqlSession.update("Idea.updateIdea", Idea);
 
 		return result;
 	}
 
-	public int deleteIdea(String IdeaId) {
+	public int deleteIdea(String IdeaId) {// 게시글 삭제
 		int result = sqlSession.delete("Idea.deleteIdea", IdeaId);
 
 		return result;
 	}
 
-	public List<IdeaVo> selectIdeas(String startDt, String endDt) {
-
-		List<IdeaVo> result = sqlSession.selectList("Idea.selectIdeas", params);
+	public List<IdeaVo> selectIdeas() {// 게시글 목록 보기
+		List<IdeaVo> result = sqlSession.selectList("Idea.selectIdeas");
 
 		return result;
 	}
 
-	public IdeaVo selectIdea(String IdeaId) {
-		IdeaVo Idea = sqlSession.selectOne("Idea.selectIdea", IdeaId);
+	public IdeaVo selectIdea(String idxNum) {// 상세 보기
+		IdeaVo Idea = sqlSession.selectOne("Idea.selectIdea", idxNum);
 
 		return Idea;
 	}
-
-	public int countIdea(String startDt, String endDt) {
-
-		Map params = new HashMap();
-		params.put("startDt", startDt);
-		params.put("endDt", endDt);
-
-		int result = sqlSession.selectOne("Idea.countIdea", params);
-
-		return result;
-	}
+	
 }
--- src/main/java/com/hanbit/team04/service/IdeaService.java
+++ src/main/java/com/hanbit/team04/service/IdeaService.java
@@ -2,13 +2,63 @@ package com.hanbit.team04.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.hanbit.team04.core.IdeaVo;
+import com.hanbit.team04.dao.IdeaDao;
 
+@Service
 public class IdeaService {
-private int addIdea;
-private  List<IdeaVo> getIdeaList;
-private  IdeaVo getIdeaone;
-private  int modifyIdea;
-private  int deleteIdea;
-private  List<String> searchIdea;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(IdeaService.class);
+	
+	@Autowired
+	IdeaDao ideaDao = new IdeaDao();
+	
+	public int addIdea(IdeaVo Idea) {
+		LOGGER.debug("게시글 추가");
+		return ideaDao.insertIdea(Idea);
+	}
+
+	public int modifyIdea(IdeaVo Idea) {
+		LOGGER.debug("게시글 수정");
+		return ideaDao.updateIdea(Idea);
+	}
+
+	public int removeSchedule(String IdeaId) {
+		LOGGER.debug("게시글 삭제");
+		return ideaDao.deleteIdea(IdeaId);
+	}
+
+	public List<IdeaVo> listSchedules() {
+		LOGGER.debug("게시글 목록보기");
+		return ideaDao.selectIdeas();
+	}
+
+	public IdeaVo getDetailBoard(String idxNum) {
+		return ideaDao.selectIdea(idxNum);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private int addIdea;
+	private  List<IdeaVo> getIdeaList;
+	private  IdeaVo getIdeaone;
+	private  int modifyIdea;
+	private  int deleteIdea;
+	private  List<String> searchIdea;
+
+
+
+
+
+
 }
