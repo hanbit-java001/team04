6ecd69a5744189fa97837fb16530d044e7238dfa
parkjugod <pjh_1987@naver.com>
2016-11-03 22:12:38 +0900

    asdsad

---
+++ src/main/java/com/hanbit/team04/web/controller/Test.java
@@ -0,0 +1,69 @@
+package com.hanbit.team04.web.controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+public class Test {
+
+
+//	 public static void main(String args[]) throws Exception{
+//		    // 이 팩토리인스턴스는 재이용가능하고 thread safe합니다.
+//		    Twitter twitter = TwitterFactory.getSingleton();
+//		    twitter.setOAuthConsumer("u5X62WknsxBFP255T5gd2gY49", "s58ZsQ4OKPEZq4fonZDUqnOXYpBpLxdWXSirgyvXYRZfHJnblC");
+//		    RequestToken requestToken = twitter.getOAuthRequestToken();
+//		    AccessToken accessToken = null;
+//		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		    while (null == accessToken) {
+//		      System.out.println("Open the following URL and grant access to your account:");
+//		      System.out.println(requestToken.getAuthorizationURL());
+//		      System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+//		      String pin = br.readLine();
+//		      try{
+//		         if(pin.length() > 0){
+//		           accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+//		         }else{
+//		           accessToken = twitter.getOAuthAccessToken();
+//		         }
+//		      } catch (TwitterException te) {
+//		        if(401 == te.getStatusCode()){
+//		          System.out.println("Unable to get the access token.");
+//		        }else{
+//		          te.printStackTrace();
+//		        }
+//		      }
+//		    }
+//		    //향후에 참조용으로 accessToken 을 지속시킨다.
+//		    storeAccessToken(twitter.verifyCredentials().getId() , accessToken);
+//		    Status status = twitter.updateStatus(args[0]);
+//		    System.out.println("Successfully updated the status to [" + status.getText() + "].");
+//		    System.exit(0);
+//		  }
+//		  private static void storeAccessToken(long l, AccessToken accessToken){
+//		    //accessToken.getToken() 을 보존
+//		    //accessToken.getTokenSecret() 을 보존
+//		  }
+
+	public static void main(String args[]) throws Exception{
+	    // 이 팩토리인스턴스는 재이용가능하고 thread safe합니다.
+	    TwitterFactory factory = new TwitterFactory();
+	    AccessToken accessToken = loadAccessToken(Integer.parseInt("9NqGUQAAAAAAxydUAAABWCpK_Ok"));
+	    Twitter twitter = factory.getInstance();
+	    twitter.setOAuthConsumer("u5X62WknsxBFP255T5gd2gY49", "s58ZsQ4OKPEZq4fonZDUqnOXYpBpLxdWXSirgyvXYRZfHJnblC");
+	    twitter.setOAuthAccessToken(accessToken);
+	    Status status = twitter.updateStatus(args[1]);
+	    System.out.println("Successfully updated the status to [" + status.getText() + "].");
+	    System.exit(0);
+	  }
+	  private static AccessToken loadAccessToken(int useId){
+	    String token = // load from a persistent store
+	    String tokenSecret = // load from a persistent store
+	    return new AccessToken(token, tokenSecret);
+	  }
+}
--- src/main/java/com/hanbit/team04/web/controller/TwitterController.java
+++ src/main/java/com/hanbit/team04/web/controller/TwitterController.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -14,13 +16,20 @@ import twitter4j.auth.RequestToken;
 
 public class TwitterController {
 
+	private String 	AccessToken = "794120751624269824-TJMGfk4Ji5p9GQ5xLIilwA5tlWW5S61";
+	private String AccessTokenSecret ="Jdi9ToNHTfcu4s2p8wytK0Bv0Ah00WN1mOBiZpFRVpCPq";
+	private String ConsumerKey = "u5X62WknsxBFP255T5gd2gY49";
+	private String ConsumerSecret ="s58ZsQ4OKPEZq4fonZDUqnOXYpBpLxdWXSirgyvXYRZfHJnblC";
+
 	public static void main(String ar[]) {
 	    try {
-	        AccessToken accesstoken = new AccessToken("TJMGfk4Ji5p9GQ5xLIilwA5tlWW5S61", "Jdi9ToNHTfcu4s2p8wytK0Bv0Ah00WN1mOBiZpFRVpCPq");
+	        AccessToken accesstoken = new AccessToken("794120751624269824-TJMGfk4Ji5p9GQ5xLIilwA5tlWW5S61", "Jdi9ToNHTfcu4s2p8wytK0Bv0Ah00WN1mOBiZpFRVpCPq");
 	        Twitter twitter = TwitterFactory.getSingleton();
-	        twitter.setOAuthConsumer("Consumer Key (u5X62WknsxBFP255T5gd2gY49)", "Consumer Secret (s58ZsQ4OKPEZq4fonZDUqnOXYpBpLxdWXSirgyvXYRZfHJnblC)");
+	        twitter.setOAuthConsumer("u5X62WknsxBFP255T5gd2gY49", "s58ZsQ4OKPEZq4fonZDUqnOXYpBpLxdWXSirgyvXYRZfHJnblC");
 	        twitter.setOAuthAccessToken(accesstoken);
 	        User user = twitter.verifyCredentials();
+
+	        Status status1 = twitter.updateStatus("ㅇㅅㅇ");
 
 	        List<Status> list = twitter.getUserTimeline();
 	        System.out.println("타임라인 계정:"+user.getScreenName());
@@ -28,6 +37,14 @@ public class TwitterController {
 	            System.out.println("작성자:"+status.getUser().getScreenName());
 	            System.out.println("타임라인내용:"+status.getText());
 	        }
+
+//	        Twitter twitter1 = TwitterFactory.getSingleton();
+//	        Query query = new Query("졸려");
+//	        QueryResult result = twitter.search(query);
+//	        for (Status status : result.getTweets()) {
+//	            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+//	        }
+
 	    } catch (Exception e) {
 	        e.printStackTrace();
 	    }
